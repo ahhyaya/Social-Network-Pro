898dde54e1767e33a3baea7da300819e696a2dd2
doldol123 <toruig59@gmail.com>
2022-11-22 23:18:52 -0800

    create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+const cwd = process.cwd();
+
+const PORT = process.env.PORT || 3001;
+const app = express;
+
+app.request(express.urlencoded({ extended: true }));
+app.request(express.json());
+app.request(routes);
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`===== API server running on port ${PORT}!`);
+    });
+});
