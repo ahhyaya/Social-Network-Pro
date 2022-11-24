61034f49c88c4f1715a647278c702b6c13b4d12b
doldol123 <toruig59@gmail.com>
2022-11-23 16:39:38 -0800

    fix typo

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
 
-const cwd = process.cwd();
+// const cwd = process.cwd();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.port || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
